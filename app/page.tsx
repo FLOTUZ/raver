079d7fb429ffe2a1b2725f2a43bf5b500c4ecfc1
Emmanuel Esquivel <flotuz10@gmail.com>
2025-08-23 00:55:25 -0600

    - Add test event

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { HeartFilledIcon } from "@/components/icons";
 import { Button } from "@heroui/button";
+import { FiShare2 } from "react-icons/fi";
 
 export default function Home() {
   return (
@@ -6,7 +8,34 @@ export default function Home() {
       className="flex flex-col items-center justify-center gap-4 py-8"
       style={{ border: "1px solid red" }}
     >
-      <Button color="primary">Button</Button>
+      <div className="flex gap-4">
+        <div className="flex flex-col w-1/2 gap-2">
+          <h2 className="text-2xl font-bold">Evento de prueba</h2>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            voluptates.
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Button
+            color="primary"
+            size="sm"
+            variant="bordered"
+            className="flex items-center gap-1"
+          >
+            <HeartFilledIcon className="w-5 h-5" />0
+          </Button>
+          <Button
+            color="primary"
+            size="sm"
+            variant="bordered"
+            className="flex items-center gap-1"
+          >
+            <FiShare2 />
+          </Button>
+        </div>
+      </div>
+      <Button color="primary">Ver todos los eventos</Button>
     </section>
   );
 }
