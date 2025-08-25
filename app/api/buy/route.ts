2a4aacc803cc4e87fb05aa39992ff223db7b7292
Emmanuel Esquivel <flotuz10@gmail.com>
2025-08-24 22:19:39 -0600

    - Add format to PDF

--- app/api/buy/route.ts
+++ app/api/buy/route.ts
@@ -9,12 +9,16 @@ export async function POST(request: Request) {
     const ticketId = Math.floor(Math.random() * 100000).toString();
     const qrImageDataUrl = await QRCode.toDataURL(ticketId);
 
-    const pdfBuffer = await pdfGenerator("payment", {
-      name,
-      ticketId: ticketId,
-      amount: 100,
-      qrImageDataUrl,
-      whatsApp: `${whatsApp}?text=Hola%20RAVR%20-%20Instrucciones%20de%20pago%20-%20${ticketId}`,
+    const pdfBuffer = await pdfGenerator({
+      templateName: "payment",
+      width: "80mm",
+      context: {
+        name,
+        ticketId: ticketId,
+        amount: 100,
+        qrImageDataUrl,
+        whatsApp: `${whatsApp}?text=Hola%20RAVR%20-%20Instrucciones%20de%20pago%20-%20${ticketId}`,
+      },
     });
 
     const { info, previewUrl } = await sendMail({
--- lib/pdf/pdf-generator.ts
+++ lib/pdf/pdf-generator.ts
@@ -1,18 +1,30 @@
 import fs from "fs";
 import Handlebars from "handlebars";
 import { join } from "path";
-import puppeteer from "puppeteer";
+import puppeteer, { PaperFormat } from "puppeteer";
 
 /**
  * Genera un PDF a partir de un template Handlebars y devuelve un Buffer
  * @param templateName Nombre del template (.hbs) dentro de lib/mail/templates
+ * @param format Formato del PDF
+ * @param width Ancho del PDF
+ * @param height Alto del PDF
  * @param context Datos para renderizar el template
  * @returns Buffer del PDF generado
  */
-export async function pdfGenerator(
-  templateName: string,
-  context: Record<string, any>
-): Promise<Buffer> {
+export async function pdfGenerator({
+  templateName,
+  format,
+  width,
+  height,
+  context,
+}: {
+  templateName: string;
+  format?: PaperFormat;
+  width?: string | number;
+  height?: string | number;
+  context: Record<string, any>;
+}): Promise<Buffer> {
   const templatePath = join(
     process.cwd(),
     `lib/pdf/templates/${templateName}.hbs`
@@ -27,7 +39,12 @@ export async function pdfGenerator(
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
 
-  const pdfBuffer = await page.pdf({ format: "A4" });
+  const pdfBuffer = await page.pdf({
+    format,
+    width,
+    height,
+    printBackground: true,
+  });
   await browser.close();
 
   return Buffer.from(pdfBuffer);
