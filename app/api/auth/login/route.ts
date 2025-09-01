70c29c7eaa014abc17a6b4784efad49696757f9e
Emmanuel Esquivel <flotuz10@gmail.com>
2025-08-31 20:50:36 -0600

    - Add auth configs

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,22 @@
+import { signJwt } from "@/lib";
+import { prisma } from "@/prisma";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json();
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid)
+    return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+
+  const token = signJwt({
+    payload: { userId: user.id, email, name: user.name },
+  });
+
+  return NextResponse.json({ token });
+}
---
+++ interfaces/session-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface SessionPayload {
+  userId: string;
+  email: string;
+  name: string;
+}
