f3503b326dd619602ecebbe1b80af49e23b98cc2
Emmanuel Esquivel <flotuz10@gmail.com>
2025-09-07 14:25:02 -0600

    - Refactor images of test data

--- data/data.ts
+++ data/data.ts
@@ -3,9 +3,9 @@ import { Event } from "@/interfaces";
 export const events: Event[] = [
   {
     id: "1",
-    name: "🎃 Inauguración de RAVR - Noche de Disfraces 👻",
+    name: "🎃 Noche de Disfraces 2025 👻",
     description:
-      "\n\n¡La espera terminó! Celebremos la **gran inauguración de RAVR** con una fiesta de **disfraces de Halloween** que no olvidarás.  \n\nPrepárate para una noche llena de misterio, diversión y sorpresas, donde la creatividad y la magia se apoderarán del ambiente.  \n\n✨ Habrá premios para los mejores disfraces, música, y muchas experiencias aterradoras (pero divertidas).  \n\nVen con tu mejor disfraz y sé parte del inicio de esta nueva etapa.  \nLa noche será tuya… si te atreves. 🕷️🦇 \n\n📍 **Lugar:** Calle 123, Ciudad, País  \n📅 **Fecha:** 1 de enero de 2023  \n🕙 **Hora:** 10:00 AM – 12:00 PM ",
+      "\n\n¡La espera terminó! Celebremos la noche con nuestra gran **fiesta dedisfraces de Halloween 2025** que no olvidarás.  \n\nPrepárate para una noche llena de misterio, diversión y sorpresas, donde la creatividad y la magia se apoderarán del ambiente.  \n\n✨ Habrá premios para los mejores disfraces, música, y muchas experiencias aterradoras (pero divertidas).  \n\nVen con tu mejor disfraz y sé parte del inicio de esta nueva etapa.  \nLa noche será tuya… si te atreves. 🕷️🦇 \n\n📍 **Lugar:** Calle 123, Ciudad, País  \n📅 **Fecha:** 1 de enero de 2023  \n🕙 **Hora:** 10:00 AM – 12:00 PM ",
     host: "mani.codes",
     image:
       "https://i.pinimg.com/736x/70/18/12/701812553630321ca103c55afc93c172.jpg",
@@ -20,13 +20,13 @@ export const events: Event[] = [
   },
   {
     id: "2",
-    name: "Inauguración de RAVR",
+    name: "Fiesta mexicana 2025",
     description: "Inauguración de RAVR",
     host: "mani.codes",
     image:
-      "https://i.pinimg.com/736x/70/18/12/701812553630321ca103c55afc93c172.jpg",
+      "https://st.depositphotos.com/1020070/60692/v/950/depositphotos_606923168-stock-illustration-mexican-party-flyer-fiesta-mexico.jpg",
     banner:
-      "https://i.pinimg.com/736x/ab/e6/34/abe634457a1419ef24dd7443a2ffdb21.jpg",
+      "https://market-resized.envatousercontent.com/previews/files/479601040/preview.jpg?w=590&h=590&cf_fit=crop&crop=top&format=auto&q=85&s=97eab299d4e5cca051a452068579817129b90a4e0f6881443ee97fd9c643eb87",
     init_date: "2023-01-01T00:00:00.000Z",
     location: "Calle 123, Ciudad, País",
     start_time: "10:00",
