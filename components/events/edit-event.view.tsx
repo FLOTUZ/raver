e70308ec34e81abb3b1e22dd757d8b2c63d87b06
Emmanuel Esquivel <flotuz10@gmail.com>
2025-09-15 00:11:39 -0600

    - Add default value

--- components/events/edit-event.view.tsx
+++ components/events/edit-event.view.tsx
@@ -7,7 +7,7 @@ import { useFormik } from "formik";
 export const EditEventView = () => {
   const form = useFormik({
     initialValues: {
-      title: "",
+      name: "",
       description: "",
       image: "",
       banner: "",
@@ -26,59 +26,96 @@ export const EditEventView = () => {
         <div className="flex flex-col gap-8 md:min-w-[600px] min-w-[400px]">
           <Input
             isRequired
+            defaultValue={form.values.name}
             label="Name"
             labelPlacement="outside"
             name="name"
             placeholder="Nombre del evento"
+            onChange={(e) => form.setFieldValue("name", e.target.value)}
           />
 
           <Input
             isRequired
+            defaultValue={form.values.image}
             label="Image URL"
             labelPlacement="outside"
             name="image"
             placeholder="https://example.com/image.jpg"
+            onChange={(e) => form.setFieldValue("image", e.target.value)}
           />
 
           <Input
             isRequired
+            defaultValue={form.values.banner}
             label="Banner URL"
             labelPlacement="outside"
             name="banner"
             placeholder="https://example.com/image.jpg"
+            onChange={(e) => form.setFieldValue("banner", e.target.value)}
           />
 
           <DateInput
-            defaultValue={parseAbsoluteToLocal("2021-11-07T07:45:00Z")}
+            defaultValue={
+              form.values.init_date === ""
+                ? undefined
+                : parseAbsoluteToLocal(form.values.init_date)
+            }
             label={"Fecha de finalización del evento"}
             labelPlacement="outside"
             name="init_date"
+            onChange={(value) => {
+              if (value) {
+                form.setFieldValue(
+                  "init_date",
+                  new Date(value.toString()).toISOString()
+                );
+              } else {
+                form.setFieldValue("init_date", "");
+              }
+            }}
           />
 
           <DateInput
-            defaultValue={parseAbsoluteToLocal("2021-11-07T07:45:00Z")}
+            defaultValue={
+              form.values.end_date === ""
+                ? undefined
+                : parseAbsoluteToLocal(form.values.end_date)
+            }
             label={"Fecha de finalización del evento"}
             labelPlacement="outside"
             name="init_date"
+            onChange={(value) => {
+              if (value) {
+                form.setFieldValue(
+                  "end_date",
+                  new Date(value.toString()).toISOString()
+                );
+              } else {
+                form.setFieldValue("end_date", "");
+              }
+            }}
           />
 
           <Input
             isRequired
+            defaultValue={form.values.location}
             label="Location"
             labelPlacement="outside"
             name="location"
             placeholder="Ej: Auditorio Nacional, Ciudad, Pais"
+            onChange={(e) => form.setFieldValue("location", e.target.value)}
           />
 
           <Textarea
             isClearable
-            defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            defaultValue={form.values.description}
             label="Description"
             minRows={10}
             name="description"
             placeholder="Description"
             variant="bordered"
-            onClear={() => console.log("textarea cleared")}
+            onChange={(value) => form.setFieldValue("description", value)}
+            onClear={() => form.setFieldValue("description", "")}
           />
 
           <Button color="primary">Button</Button>
