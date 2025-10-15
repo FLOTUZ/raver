b6599574eac90eaee93ddd6ec40900a28fa5ba05
Emmanuel Esquivel <flotuz10@gmail.com>
2025-10-14 20:26:24 -0600

    - Add show password icon

--- components/auth/reset-password/reset-password.component.tsx
+++ components/auth/reset-password/reset-password.component.tsx
@@ -4,12 +4,18 @@ import { Button, Input } from "@heroui/react";
 import { addToast } from "@heroui/toast";
 import { useFormik } from "formik";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { IoMdEyeOff } from "react-icons/io";
+import { MdRemoveRedEye } from "react-icons/md";
 
 import { useMutation } from "@/hooks";
 
 export const ResetPasswordComponent = () => {
   const params = useSearchParams();
   const router = useRouter();
+
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const ott = params.get("ott");
   const user_id = params.get("user_id");
@@ -29,7 +35,7 @@ export const ResetPasswordComponent = () => {
     method: "POST",
     onSuccess: (_) => {
       addToast({
-        title: "Contraseña restablecida",
+        title: "Contraseña restablecida",
         variant: "solid",
         color: "success",
       });
@@ -52,10 +58,14 @@ export const ResetPasswordComponent = () => {
     validate(values) {
       const errors: any = {};
 
+      if (!values.new_password) {
+        errors.new_password = "Requerido";
+      }
+
       if (!values.confirm_password) {
         errors.confirm_password = "Requerido";
       } else if (values.confirm_password !== values.new_password) {
-        errors.confirm_password = "Las contraseñas no coinciden";
+        errors.confirm_password = "Las contraseñas no coinciden";
       }
 
       return errors;
@@ -65,7 +75,7 @@ export const ResetPasswordComponent = () => {
     onSubmit: (values) => {
       if (!ott || !user_id) {
         addToast({
-          title: "Hubo un error al restablecer la contraseña",
+          title: "Hubo un error al restablecer la contraseña",
           description: "Parametros ott y user_id son requeridos",
           variant: "solid",
           color: "danger",
@@ -83,26 +93,51 @@ export const ResetPasswordComponent = () => {
   }
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 max-w-md w-full mx-auto">
       <form className="flex flex-col gap-4" onSubmit={form.handleSubmit}>
         <Input
+          endContent={
+            <Button
+              isIconOnly
+              onPress={() => setShowNewPassword(!showNewPassword)}
+            >
+              {showNewPassword ? (
+                <MdRemoveRedEye size={20} />
+              ) : (
+                <IoMdEyeOff size={20} />
+              )}
+            </Button>
+          }
           errorMessage={form.errors.new_password}
           isInvalid={form.touched.new_password && !!form.errors.new_password}
           label="Nueva contraseña"
           name="new_password"
-          type="password"
+          type={showNewPassword ? "text" : "password"}
           value={form.values.new_password}
           onBlur={form.handleBlur}
           onChange={form.handleChange}
         />
         <Input
+          endContent={
+            <Button
+              isIconOnly
+              variant="light"
+              onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+            >
+              {showConfirmPassword ? (
+                <MdRemoveRedEye size={20} />
+              ) : (
+                <IoMdEyeOff size={20} />
+              )}
+            </Button>
+          }
           errorMessage={form.errors.confirm_password}
           isInvalid={
             form.touched.confirm_password && !!form.errors.confirm_password
           }
           label="Confirmar contraseña"
           name="confirm_password"
-          type="password"
+          type={showConfirmPassword ? "text" : "password"}
           value={form.values.confirm_password}
           onBlur={form.handleBlur}
           onChange={form.handleChange}
@@ -114,7 +149,7 @@ export const ResetPasswordComponent = () => {
           radius="full"
           type="submit"
         >
-          Restablecer contraseña
+          Restablecer contraseña
         </Button>
       </form>
     </div>
